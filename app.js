231017372f6535770bbdcccd607c2d3132eb83c0
Your Name <chantharathanaa@gmail.com>
2025-06-04 19:12:16 +0700

    updated

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ function sendGift() {
     // Open ABA payment link in a new tab
     window.open("https://pay.ababank.com/1eWAfFkCo5V5JCat8", "_blank");
 
-    // Show alert immediately (since we can't detect payment status)
+    // Show a thank you message
     setTimeout(() => {
         alert("🎉 Thanks for your gift! I really appreciate it! 🎁");
-    }, 1000); // small delay so it's not instant
+    }, 1000);
 }
